1aebd4b2f034e0bff70317bef56e182b3975d55a
Alex Widener <awidener3@gmail.com>
2021-11-03 15:28:07 -0400

    fixed bugs and added backspace and pos/neg toggle

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Setup
-let firstValue = 0;
-let secondValue = 0;
+let num1 = 0;
+let num2;
 let operation;
 let displayValue = document.querySelector('.display-value');
 let equation_div = document.querySelector('.equation');
@@ -9,6 +9,8 @@ const digit_button = document.querySelectorAll('.digit');
 const operator_button = document.querySelectorAll('#operator');
 const equals_button = document.querySelector('#equals');
 const decimal_button = document.querySelector('.decimal');
+const backspace_button = document.querySelector('#backspace');
+const toggle_button = document.querySelector('#toggle-neg');
 
 // basic functions
 
@@ -40,24 +42,37 @@ function operate (operator, num1, num2) {
     switch (operator) {
         case '+':
             displayValue.textContent = checkForDecimals(add(num1,num2));
-            break;
+            return add(num1, num2);
         case '-':
             displayValue.textContent = checkForDecimals(subtract(num1,num2));
-            break;
+            return subtract(num1, num2);
         case '*':
             displayValue.textContent = checkForDecimals(multiply(num1,num2));
-            break;
+            return multiply(num1, num2);
         case '/':
             if (num2 == 0) {
                 displayValue.textContent = 'ERROR';
+                return 'ERROR';
             } else {
                 displayValue.textContent = checkForDecimals(divide(num1, num2));
-            }
-            break;
+                return divide(num1,num2);
+            } 
     }
 }
 
-// DIGITS
+function onPressBackspace() {
+    displayValue.textContent = displayValue.textContent.substring(0, displayValue.textContent.length - 1);
+}
+
+function togglePositiveToNegative(num) {
+    if (Math.sign(num) >= 0) {
+        displayValue.textContent = -Math.abs(num);
+    } else {
+        displayValue.textContent = Math.abs(num);
+    }
+}
+
+// DIGIT BUTTONS //
 
 for (let i = 0; i < digit_button.length; i++) {
     digit_button[i].addEventListener('click', function() {
@@ -69,30 +84,51 @@ for (let i = 0; i < digit_button.length; i++) {
     })
 }
 
-// OPERATIONS //
+// OPERATION BUTTONS //
 
 for (let i = 0; i < operator_button.length; i++) {
     operator_button[i].addEventListener('click', function() {
-        firstValue = displayValue.textContent;   // set 'a' to current displayed number
-        operation = this.value;         // assign the operator to 'operation'
-        displayValue.textContent = '';  // reset displayed number to empty
-        equation_div.textContent = `${firstValue} ${operation}`; 
+        num1 = displayValue.textContent;    // set 'num1' to current displayed number
+        equals_button.disabled = false;
+        if (num2 !== undefined) {
+            num2 = operate(operation, num2, num1);
+            
+        } else {
+            num2 = num1;
+        }
+        
+        operation = this.value;             // assign the operator to 'operation'
+        equation_div.textContent = `${num2} ${operation}`; 
+        displayValue.textContent = '';      // reset displayed number to empty
         decimal_button.disabled = false;
     })
 }
 
-equals_button.addEventListener('click', () => operate(operation, firstValue));
+equals_button.addEventListener('click', function() { 
+    operate(operation, num1, num2);
+    equals_button.disabled=true;
+});
 
-// clear & check button
+// CLEAR BUTTON //
 
 clear_button.addEventListener('click', () => {
     displayValue.textContent = '';      // reset displayed number to empty
     equation_div.textContent = '';      // reset equation display
-    a = undefined;                      // reset 'a' to undefined
+    num1 = undefined;                      // reset 'a' to undefined
+    num2 = undefined;
     operation = undefined;              // reset 'operation' to undefined
     decimal_button.disabled = false;
+    equals_button.disabled = false;
 });
 
-// Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
+// BACKSPACE BUTTON //
 
-// You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
+backspace_button.addEventListener('click', function() {
+    onPressBackspace();
+})
+
+// TOGGLE POS AND NEG //
+
+toggle_button.addEventListener('click', function() {
+    togglePositiveToNegative(displayValue.textContent);
+})
