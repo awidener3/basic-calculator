e3a647b1c49870b74a0207285cd8cafbdf1ebe87
Alex Widener <awidener3@gmail.com>
2021-11-02 20:46:04 -0400

    fixed decimals/disabled decimal buttton to prevent multiple uses

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 // Setup
-let a;
+let firstValue = 0;
+let secondValue = 0;
 let operation;
-let displayValue = document.querySelector('.selection');
+let displayValue = document.querySelector('.display-value');
 let equation_div = document.querySelector('.equation');
 const clear_button = document.querySelector('#clear')
 const digit_button = document.querySelectorAll('.digit');
 const operator_button = document.querySelectorAll('#operator');
 const equals_button = document.querySelector('#equals');
-// const check_button = document.querySelector('.check');
+const decimal_button = document.querySelector('.decimal');
 
 // basic functions
 
 function add (a, b) {
     return Number(a) + Number(b); // convert returned strings as numbers before adding
 }
-
 function subtract (a, b) {
-    return a - b;
-}
+    return a - b;}
 
 function multiply (a, b) {
     return a * b;
 }
-
 function divide (a, b) {
     return a / b;
+}
+function checkForDecimals(result) {
+    if (result % 1 != 0) {
+        return result.toFixed(2);
+    } else {
+        return result;
+    }
 }
 
 // calculator functionality
@@ -34,23 +39,49 @@ function operate (operator, num1, num2) {
     num2 = displayValue.textContent;    // assign the current displayed number to num2
     switch (operator) {
         case '+':
-            displayValue.textContent = add(num1,num2).toFixed(9);
+            displayValue.textContent = checkForDecimals(add(num1,num2));
             break;
         case '-':
-            displayValue.textContent = subtract(num1,num2).toFixed(9);
+            displayValue.textContent = checkForDecimals(subtract(num1,num2));
             break;
         case '*':
-            displayValue.textContent = multiply(num1,num2).toFixed(9);
+            displayValue.textContent = checkForDecimals(multiply(num1,num2));
             break;
         case '/':
             if (num2 == 0) {
                 displayValue.textContent = 'ERROR';
             } else {
-                displayValue.textContent = divide(num1, num2).toFixed(9);
+                displayValue.textContent = checkForDecimals(divide(num1, num2));
             }
             break;
     }
 }
+
+// DIGITS
+
+for (let i = 0; i < digit_button.length; i++) {
+    digit_button[i].addEventListener('click', function() {
+        displayValue.textContent += this.textContent;   // add numbers to the displayed number field
+        // disable decimal button if a decimal is present
+        if (displayValue.textContent.includes('.')) {
+            decimal_button.disabled = true;
+        }
+    })
+}
+
+// OPERATIONS //
+
+for (let i = 0; i < operator_button.length; i++) {
+    operator_button[i].addEventListener('click', function() {
+        firstValue = displayValue.textContent;   // set 'a' to current displayed number
+        operation = this.value;         // assign the operator to 'operation'
+        displayValue.textContent = '';  // reset displayed number to empty
+        equation_div.textContent = `${firstValue} ${operation}`; 
+        decimal_button.disabled = false;
+    })
+}
+
+equals_button.addEventListener('click', () => operate(operation, firstValue));
 
 // clear & check button
 
@@ -59,34 +90,8 @@ clear_button.addEventListener('click', () => {
     equation_div.textContent = '';      // reset equation display
     a = undefined;                      // reset 'a' to undefined
     operation = undefined;              // reset 'operation' to undefined
+    decimal_button.disabled = false;
 });
-
-    // check_button.addEventListener('click', () => {
-    //     console.log(`${a} ${operation} ${b}`);
-    // })
-
-// digit buttons
-
-for (let i = 0; i < digit_button.length; i++) {
-    digit_button[i].addEventListener('click', function() {
-        displayValue.textContent += this.textContent;   // add numbers to the displayed number field
-    })
-}
-
-// operator buttons
-
-for (let i = 0; i < operator_button.length; i++) {
-    operator_button[i].addEventListener('click', function() {
-        a = displayValue.textContent;   // set 'a' to current displayed number
-        displayValue.textContent = '';  // reset displayed number to empty
-        operation = this.value;         // assign the operator to 'operation'
-        equation_div.textContent = `${a} ${operation}`
-    })
-}
-
-equals_button.addEventListener('click', () => operate(operation, a));
-
-
 
 // Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
 
